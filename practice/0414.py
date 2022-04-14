78fb4c8a5c02c4eec35b99eab862273064fb62df
cheernow2022 <947072026@qq.com>
2022-04-14 21:06:20 +0800

    Default Changelist

---
+++ practice/0414.py
@@ -0,0 +1,17 @@
+# -*- coding=utf-8 -*-
+
+def maopao(list):
+    n = len(list)
+    for i in range(0,n-1):
+        for j in range(0,n-1-i):
+            if list[j] > list[j+1]:
+                list[j],list[j+1] = list[j+1],list[j]
+
+if __name__ == '__main__':
+    list = [1,6,5,3,8]
+    maopao(list)
+    print list
+
+
+
+
