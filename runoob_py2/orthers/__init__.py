3d7c073a575fd5e26b5f714785b106f8d52736b9
cheernow2022 <947072026@qq.com>
2022-04-16 17:13:27 +0800

    4月16日下午，从py0305，全推送

---
+++ runoob_py2/orthers/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding=utf-8 -*-
+
+if __name__ == '__main__':
+    print '作为主程序运行'
+else:
+    print 'package_runoob 初始化'
---
+++ runoob_py2/orthers/support.py
@@ -0,0 +1,12 @@
+# -*- coding=utf-8 -*-
+
+def print_func( par ):
+   print "Hello : ", par
+   return
+
+def runoob1():
+   print "I'm in runoob1"
+
+def runoob2():
+   print "I'm in runoob2"
+
