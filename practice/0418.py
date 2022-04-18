d94699c9030cc9b323d916ee198237cbfd686a49
cheernow2022 <947072026@qq.com>
2022-04-18 23:27:38 +0800

    4月18日，提交9

--- practice/0418.py
+++ practice/0418.py
@@ -1,13 +1,13 @@
 # -*- coding=utf-8 -*-
 
-# print('hello world')
-# print('hello234world')
-# print('hello\tworld')
-# print('hello\rworld')
-# print(';大家好;')
-# print(' ’大家好‘ ')
+print('hello world')
+print('hello234world')
+print('hello\tworld')
+print('hello\rworld')
+print(';大家好;')
+print(' ’大家好‘ ')
 
-# print('https:\\\\www.baibu.com')
+
 
 
 
